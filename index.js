3670a07c7f19df76b187ef33a655c35d4016f530
Cassandra Umeh <umehcassandra@gmail.com>
2022-07-24 18:12:59 +0100

    Add files via upload
    build a search bar that searches and displays user name

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+    Task: Comparing the search query vs. the DOM names
+    1. Create a conditional that checks if currentName is equal to the user's search query!
+    2. If our conditional returns true, set the display style on 'allNamesDOMCollection[counter]' to be a block element
+    3. If our conditional falls into the else, we have no match! So set the display style to 'none'
+*/
+document.getElementById("searchInput").addEventListener("keyup", function(event) {
+    let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName('name');
+    
+    for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
+        const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
+
+        
+        if(currentName.includes(searchQuery)){
+            allNamesDOMCollection[counter].style.display = "block";
+            }else{
+                allNamesDOMCollection[counter].style.display = "none";
+            }
+    }
+});
